b4d7861759c0cdf752e01d7cbd785a59a5b52a53
AmitM <AmitM@Lenovo-ideapad320>
2020-11-14 16:44:50 +0530

    welcome message added

---
+++ src/calculateEmployeeWage/CalculateEmployeeWage.java
@@ -0,0 +1,13 @@
+package calculateEmployeeWage;
+
+public class CalculateEmployeeWage {
+//	Display Welcome Message....
+	static {
+		System.out.println("___________Welcome to Employee Wage Computation___________");
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
